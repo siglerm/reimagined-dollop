b5f49d470cedad8f425cd5b2fb9b44c40baef160
Matt <siglerm37@gmail.com>
2023-06-07 19:23:48 -0700

    Added about page routing

--- the-inbetween/src/pages/index.tsx
+++ the-inbetween/src/pages/index.tsx
@@ -1,19 +1,19 @@
 import Link from "next/link";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import styles from "../styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function HomePage() {
+export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.homePage}>
       <h1>Welcome to the Inbetween</h1>
       <p>
         A land a bit different from ours, where the familiar is few and far
         between.
       </p>
-      <Link href={"/scene/1"}>
-        <a className="start-button">Start your Journey</a>
+      <Link href={""}>
+        <button className={styles.startButton}>Start your Journey</button>
       </Link>
     </div>
   );
